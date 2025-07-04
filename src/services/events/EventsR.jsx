6fa80678647d33d366966c1718ed2cef8b8c43a9
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2025-07-04 16:57:29 -0300

    Cargar imagenes de eventos

--- src/services/events/EventsR.jsx
+++ src/services/events/EventsR.jsx
@@ -64,7 +64,7 @@ export default function EventsR( data ) {
 
                         {
                             dataEvents.map( (event) => (
-                                <div key={event.id} className={`item ${event.slug}`}>
+                                <div key={event.id} className={`item ${event.slug}`} style={{ backgroundImage: `url(../../public/events/${event.data.img})` }}>
                                     <div className="content">
                                         <div className="name">{event.data.titulo}</div>
                                         <div className="site">{event.data.lugar}</div>
