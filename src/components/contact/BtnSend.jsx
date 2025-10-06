3cde82f91377fbab1acbbac4eff7c4964905c0c4
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2025-10-06 14:54:50 -0300

    Activar formulario de envio contacto

--- src/components/contact/BtnSend.jsx
+++ src/components/contact/BtnSend.jsx
@@ -1,13 +1,13 @@
 
 import "../../styles/contact/BtnSend.css";
 
-export default function BtnBuy({ children, tipo }) {
+export default function BtnBuy({ children, tipo, func }) {
 
     
     return (
 
         <>
-             <button type={tipo} className="btn-send"><span>{children}</span><i></i></button>
+             <button type={tipo} className="btn-send" onClick={func}><span>{children}</span><i></i></button>
         </>
     )
 }  
--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,8 +1,71 @@
+import { use, useState } from "react";
 import "../../styles/contact/Form.css";
 import BtnSend from "./BtnSend";
 
 
 export default function Form() {
+
+    const initialForm = {
+        nombre: "",
+        email:"",
+        telefono:"",
+        mensaje:""
+    }
+    const [form, setForm] = useState(initialForm);
+    const [message, setMessage] = useState({
+        message: "",
+        color: ""
+    });
+
+    const onInputChange = ( {target} )=> {
+
+        const {name, value} = target;
+        setForm({
+            ...form,
+            [name]: value,
+        })
+
+    }
+
+    const submit = async (e) => {
+            e.preventDefault();
+            try {
+                    let objectSubmit = {
+                
+                        method : "POST",
+                        body : JSON.stringify(form),
+                        headers : {
+                            "Content-type" : "application/json",
+                            "Accept": "aplication/json"
+                        } 
+                    }
+
+                    const JSONSubmit = await fetch("https://formsubmit.co/ajax/essencecrew.bookings@gmail.com",objectSubmit);
+                    const data = await JSONSubmit.json();
+
+                    if(data.success) {
+                        setForm(initialForm)
+
+                        setMessage({
+                            message: "Mensaje Enviado",
+                            color: "green"
+                        })
+                    } else {
+                            setMessage({
+                            message: "Mensaje no enviado",
+                            color: "red"
+                        })
+                    }
+            } catch {
+                
+                        setMessage({
+                            message: "Error de coenxion",
+                            color: "red"
+                        })
+            }
+    }
+
+
 
     return (
                 <div className="contactForm">
@@ -10,25 +73,28 @@ export default function Form() {
                     <form>
                         <h2>Envianos tu mensaje</h2>
                         <div className="inputBox">
-                            <input type="text" name="" required="required" />
+                            <input type="text" name="nombre" required="required" onChange={onInputChange} value={form.name}/>
                             <span className="form-span">Nombre</span>
                         </div>
                         <div className="inputBox">
-                            <input type="text" name="" required="required" />
+                            <input type="text" name="email" required="required" onChange={onInputChange} value={form.email}/>
                             <span className="form-span">Correo</span>
                         </div>
                         <div className="inputBox">
-                            <input type="text" name="" required="required" />
+                            <input type="text" name="telefono" required="required" onChange={onInputChange} value={form.phone}/>
                             <span className="form-span">Telefono</span>
                         </div>
                         <div className="inputBox">
-                            <textarea required="required"></textarea>
+                            <textarea required="required" name="mensaje" onChange={onInputChange} value={form.text}></textarea>
                             <span className="form-span">Escribe su Mansaje</span>
                         </div>
+                        <div className="box-message ">
+                            <h3 className={message.color}>{message.message}</h3>
+                        </div>
                         <div className="inputBox box-btn-send">
-
                             <BtnSend
                                 tipo="submit"
+                                func = {submit}
                             >Enviar</BtnSend>
                         </div>
                     </form>
