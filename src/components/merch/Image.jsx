ae4d5c742cf0c664fb29f05b1544269655c607d8
Mariano Sy <138610830+marianos1988@users.noreply.github.com.>
2025-12-09 13:46:21 -0300

    correccion componente Image

--- src/components/merch/Image.jsx
+++ src/components/merch/Image.jsx
@@ -3,8 +3,24 @@
 export default function Image({ id, img }) {
     console.log(`/products/${id}/${img}`)
     const path = `/products/${id}/${img}`;
+
+
+// const images = import.meta.glob('../assets/products/*.{png,jpg,jpeg,webp}', {
+//   eager: true
+// });
+
+// const id = '1.jpg';
+// const imgSrc = images[`../assets/productos/${id}`].src;
+
+
+// <img src={imgSrc} alt="Producto" />
+
+
+
+
+
     return(
         // <img src={`./products/${id}/${img}`} alt="" />
-        <img src={path} alt={id} />
+        <img src="/products/producto-1/img1.jpg" alt={id} />
     )
 } 
